5d1491ed6ab49044cda329d8f621a88195ad8777
limminho94 <dla47941343@gmail.com>
2024-12-28 16:11:05 +0900

    1228 로봇팔 프로젝트 tcp 구현중

--- project_can/MainWindow.xaml.cs
+++ project_can/MainWindow.xaml.cs
@@ -1,49 +1,30 @@
 ﻿using System;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
-using System.Net.Sockets;
-using static System.Net.Mime.MediaTypeNames;
-
-
+using System.Windows.Threading;
 using System.Drawing;
 using System.IO;
-using System.Windows.Threading;
-
+using System.Net;
+using System.Net.Sockets;
 using OpenCvSharp;
 using OpenCvSharp.Extensions;
-using System.Net.Sockets;
-using System.Net;
+using System.Threading;
+using System.Windows.Media.Imaging;
 
 namespace project_can
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : System.Windows.Window
     {
-        private VideoCapture capVideo;
         private VideoCapture capCamera;
-        
-        Mat matImage = new Mat();
+        private Mat matImage = new Mat();
+        private ClientHandler clientHandler;
 
         public MainWindow()
         {
             InitializeComponent();
             InitializeCamera();
-            SendToServer();
+            clientHandler = new ClientHandler();
+            clientHandler.ConnectToServer();
         }
 
         private void InitializeCamera()
@@ -53,7 +34,6 @@ namespace project_can
 
         private void btPlay_Click(object sender, RoutedEventArgs e)
         {
-            btPlay.IsEnabled = false;
             new Thread(PlayCamera).Start();
         }
 
@@ -61,12 +41,19 @@ namespace project_can
         {
             while (!capCamera.IsDisposed)
             {
-                capCamera.Read(matImage); 
-                if (matImage.Empty()) break;
+                capCamera.Read(matImage);
+                if (matImage.Empty())
+                {
+                    break;
+                }
                 Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
                 {
                     var converted = Convert(BitmapConverter.ToBitmap(matImage));
                     imgViewport.Source = converted;
+
+                    // 프레임 전송
+                    byte[] frameData = matImage.ToBytes();
+                    clientHandler.SendFrameToServer(frameData);
                 }));
             }
         }
@@ -77,8 +64,7 @@ namespace project_can
             {
                 capCamera.Dispose();
             }
-            btStop.IsEnabled = false;
-            
+
             this.Close();
         }
 
@@ -93,33 +79,80 @@ namespace project_can
             image.EndInit();
             return image;
         }
+    }
 
-        // 소켓을 생성한다.
-        public void SendToServer()
+    public class ClientHandler
+    {
+        private Socket client;
+
+        public void ConnectToServer()
         {
-            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            // Connect 함수로 로컬(127.0.0.1)의 포트 번호 9999로 대기 중인 socket에 접속한다.
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             client.Connect(new IPEndPoint(IPAddress.Parse("10.10.20.116"), 12345));
-            // 보낼 메시지를 UTF8타입의 byte 배열로 변환한다.
-            var data = Encoding.UTF8.GetBytes("클라이언트에서 보내는 메세지");
+        }
 
-            // big엔디언으로 데이터 길이를 변환하고 서버로 보낼 데이터의 길이를 보낸다. (4byte)
-            client.Send(BitConverter.GetBytes(data.Length));
-            // 데이터를 전송한다.
-            client.Send(data);
+        //public void SendMessageToServer(string message)
+        //{
+        //    var data = Encoding.UTF8.GetBytes(message);
+        //    byte[] header = Encoding.UTF8.GetBytes("m");
+        //    byte[] lengthBytes = BitConverter.GetBytes((ushort)data.Length);
+        //    if (BitConverter.IsLittleEndian)
+        //        Array.Reverse(lengthBytes);
+        //    client.Send(header);
+        //    client.Send(lengthBytes);
+        //    client.Send(data);
+        //}
 
-            // 데이터의 길이를 수신하기 위한 배열을 생성한다. (4byte)
-            data = new byte[4];
-            // 데이터의 길이를 수신한다.
-            client.Receive(data, data.Length, SocketFlags.None);
-            // server에서 big엔디언으로 전송을 했는데도 little 엔디언으로 온다. big엔디언과 little엔디언은 배열의 순서가 반대이므로 reverse한다.
-            Array.Reverse(data);
-            // 데이터의 길이만큼 byte 배열을 생성한다.
-            data = new byte[BitConverter.ToInt32(data, 0)];
-            // 데이터를 수신한다.
-            client.Receive(data, data.Length, SocketFlags.None);
-            // 수신된 데이터를 UTF8인코딩으로 string 타입으로 변환 후에 콘솔에 출력한다.
-            Console.WriteLine(Encoding.UTF8.GetString(data));
+        public void SendFrameToServer(byte[] frameData)
+        {
+            try
+            {
+                if (client.Connected)
+                {
+                    byte[] header = Encoding.UTF8.GetBytes("img");
+                    byte[] lengthBytes = BitConverter.GetBytes((ushort)frameData.Length);
+                    if (BitConverter.IsLittleEndian)
+                        Array.Reverse(lengthBytes);
+                    client.Send(header);
+                    client.Send(lengthBytes);
+                    client.Send(frameData);
+                }
+                else
+                {
+                    Console.WriteLine("연결이 종료되었습니다. 재연결 시도 중...");
+                    ConnectToServer(); // 재연결 시도
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine($"소켓 오류: {ex.Message}");
+                ConnectToServer(); // 연결 재시도
+            }
+        }
+
+        public string ReceiveMessageFromServer()
+        {
+            byte[] header = new byte[3];
+            client.Receive(header, 3, SocketFlags.None);
+            string dataType = Encoding.UTF8.GetString(header[0..1]);
+            ushort length = BitConverter.ToUInt16(header[1..3], 0);
+
+            if (BitConverter.IsLittleEndian)
+            {
+                byte[] lengthBytes = header[1..3];
+                Array.Reverse(lengthBytes);
+                length = BitConverter.ToUInt16(lengthBytes, 0);
+            }
+
+            byte[] data = new byte[length];
+            client.Receive(data, length, SocketFlags.None);
+
+            if (dataType == "m")
+            {
+                return Encoding.UTF8.GetString(data);
+            }
+
+            return string.Empty;
         }
     }
 }
